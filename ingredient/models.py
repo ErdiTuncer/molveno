2f1da6bd713b33568aa077bd01e717840e71d642
Michael Akkermans <info@makkermans.nl>
2018-03-20 15:48:50 +0100

    Created ingredient first draft

--- ingredient/models.py
+++ ingredient/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    """..."""
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+    stock_required = models.IntegerField()
+    stock_total = models.IntegerField()
+    type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
